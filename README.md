fab416590d24867a89300fe9c32f3ad9479bccf6
reta <drreta@gmail.com>
2019-07-18 11:32:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Testing Spring Boot conditionals the sane way
+===
+
+Sample project to illustrate testing the Spring Boot conditionals for 1.5.x and 2.1.x projects, with JUnit 4 or JUnit 5.  
+
+  - branch `sb15.junit.4`: Spring Boot 1.5.x and JUnit 4
+
+
+  - branch `sb15.junit.5`: Spring Boot 1.5.x and JUnit 5
+
+
+  - branch `sb21.junit.4`: Spring Boot 2.1.x and JUnit 4
+  
+ 
+  - branch `master`: Spring Boot 2.1.x and JUnit 5
---
+++ src/test/java/com/example/spring/boot/config/LoggingConfigurationTest.java
@@ -0,0 +1,31 @@
+package com.example.spring.boot.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.helpers.NOPLogger;
+import org.springframework.boot.context.annotation.UserConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+public class LoggingConfigurationTest {
+    private final ApplicationContextRunner runner = new ApplicationContextRunner()
+        .withConfiguration(UserConfigurations.of(LoggingConfiguration.class));
+    
+    @Test
+    public void loggerShouldBeSlf4j() {
+        runner
+            .run(ctx -> 
+                assertThat(ctx.getBean(Logger.class)).isInstanceOf(Logger.class)
+            );
+    }
+    
+    @Test
+    public void loggerShouldBeNoop() {
+        runner
+            .withPropertyValues("logging.enabled=false")
+            .run(ctx -> 
+                assertThat(ctx.getBean(Logger.class)).isSameAs(NOPLogger.NOP_LOGGER)
+            );
+    }
+}
